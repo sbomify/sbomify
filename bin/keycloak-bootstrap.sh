b8fe8f9f9f4e079063de029cd8fe937f3dd1f0d5
Viktor Petersson <self@vpetersson.com>
2025-05-14 16:02:07 +0100

    Clean up Keycloak bootstrap script and streamline Docker Compose configs
    - Update bin/keycloak-bootstrap.sh to remove default fallbacks, use explicit env vars and add quoting around all kcadm.sh parameters
    - Simplify start_development_environment.sh to a single `docker-compose up -d --build` invocation
    - Add dependency on `socialaccount 0004_app_provider_id_settings` in migration 0004_create_keycloak_social_app.py
    - Remove legacy docker-compose.prod.yml
    - Simplify docker-compose.dev.yml by dropping common/keycloak env anchors and unused Keycloak services
    - Revamp docker-compose.yml: introduce reusable anchors for common, Keycloak admin, Keycloak, Minio and Django env; consolidate service env settings, volumes and healthchecks

--- bin/keycloak-bootstrap.sh
+++ bin/keycloak-bootstrap.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 set -e
 
-KC_URL="${KC_URL:-http://keycloak:8080}"
-REALM="${KEYCLOAK_REALM:-sbomify}"
-CLIENT_ID="${KEYCLOAK_CLIENT_ID:-sbomify}"
-ADMIN_USER="${KEYCLOAK_ADMIN_USERNAME:-admin}"
-ADMIN_PASS="${KEYCLOAK_ADMIN_PASSWORD:-admin}"
-CLIENT_SECRET="${KEYCLOAK_CLIENT_SECRET:-dev-client-secret}"
+KC_URL="${KEYCLOAK_SERVER_URL}"
+REALM="${KEYCLOAK_REALM}"
+CLIENT_ID="${KEYCLOAK_CLIENT_ID}"
+ADMIN_USER="${KC_BOOTSTRAP_ADMIN_USERNAME}"
+ADMIN_PASS="${KC_BOOTSTRAP_ADMIN_USERNAME}"
+CLIENT_SECRET="${KEYCLOAK_CLIENT_SECRET}"
 
 # Wait for Keycloak REST API to be ready
-until /opt/keycloak/bin/kcadm.sh config credentials --server $KC_URL --realm master --user $ADMIN_USER --password $ADMIN_PASS; do
+until /opt/keycloak/bin/kcadm.sh config credentials --server "$KC_URL" --realm master --user "$ADMIN_USER" --password "$ADMIN_PASS"; do
   echo "Waiting for Keycloak to be ready for admin CLI..."
   sleep 3
 done
 
 # Create realm if it doesn't exist
-if ! /opt/keycloak/bin/kcadm.sh get realms/$REALM > /dev/null 2>&1; then
+if ! /opt/keycloak/bin/kcadm.sh get "realms/$REALM" > /dev/null 2>&1; then
   /opt/keycloak/bin/kcadm.sh create realms \
-    -s realm=$REALM \
+    -s "realm=$REALM" \
     -s enabled=true
 fi
 
 # Create client if it doesn't exist
-if ! /opt/keycloak/bin/kcadm.sh get clients -r $REALM -q clientId=$CLIENT_ID | grep -q '"id"'; then
-  /opt/keycloak/bin/kcadm.sh create clients -r $REALM \
-    -s clientId=$CLIENT_ID \
+if ! /opt/keycloak/bin/kcadm.sh get clients -r "$REALM" -q "clientId=$CLIENT_ID" | grep -q '"id"'; then
+  /opt/keycloak/bin/kcadm.sh create clients -r "$REALM" \
+    -s "clientId=$CLIENT_ID" \
     -s enabled=true \
     -s protocol=openid-connect \
     -s publicClient=false \
@@ -33,13 +33,13 @@ if ! /opt/keycloak/bin/kcadm.sh get clients -r $REALM -q clientId=$CLIENT_ID | g
     -s standardFlowEnabled=true \
     -s directAccessGrantsEnabled=true \
     -s serviceAccountsEnabled=true \
-    -s secret=$CLIENT_SECRET
+    -s "secret=$CLIENT_SECRET"
 else
-  CLIENT_UUID=$(/opt/keycloak/bin/kcadm.sh get clients -r $REALM -q clientId=$CLIENT_ID --fields id --format csv | tail -n1 | tr -d '"')
-  /opt/keycloak/bin/kcadm.sh update clients/$CLIENT_UUID -r $REALM -s secret=$CLIENT_SECRET
+  CLIENT_UUID=$(/opt/keycloak/bin/kcadm.sh get clients -r "$REALM" -q clientId="$CLIENT_ID" --fields id --format csv | tail -n1 | tr -d '"')
+  /opt/keycloak/bin/kcadm.sh update "clients/$CLIENT_UUID" -r "$REALM" -s secret="$CLIENT_SECRET"
 fi
 
 # Always enable user registration after all other steps
-/opt/keycloak/bin/kcadm.sh update realms/$REALM -s registrationAllowed=true
+/opt/keycloak/bin/kcadm.sh update "realms/$REALM" -s registrationAllowed=true
 
 echo "Keycloak client secret for $CLIENT_ID: $CLIENT_SECRET"
