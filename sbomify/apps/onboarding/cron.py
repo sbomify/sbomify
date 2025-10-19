9984cf5b322a5be64566d9f5608c8e794462818b
Viktor Petersson <self@vpetersson.com>
2025-10-19 15:30:34 +0100

    Fixes missing decorator

--- sbomify/apps/onboarding/cron.py
+++ sbomify/apps/onboarding/cron.py
@@ -5,6 +5,7 @@ This module defines the periodic tasks for sending onboarding emails.
 These tasks are designed to work with dramatiq-crontab for scheduling.
 """
 
+import dramatiq
 from dramatiq_crontab import cron
 
 from .tasks import process_all_onboarding_reminders_task
@@ -13,6 +14,7 @@ from .tasks import process_all_onboarding_reminders_task
 # Schedule onboarding reminder processing to run daily at 9:00 AM UTC
 # This will process first component/SBOM reminders that adapt based on user progress
 @cron("0 9 * * *")  # Daily at 9:00 AM UTC
+@dramatiq.actor(queue_name="onboarding_cron", max_retries=1, time_limit=600000)
 def daily_onboarding_reminders():
     """
     Daily task to process all onboarding reminder emails.
