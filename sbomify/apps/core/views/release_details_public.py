80d1bb9e69ccb4eb9c05a8ec72bf9f0dfbdb4cad
Rusko124 <psafronov@screenly.io>
2025-10-19 16:43:55 +0400

    Enhance ReleaseDetailsPublicView to include error handling for non-existent releases. Import get_release function within the view method for improved encapsulation.

--- sbomify/apps/core/views/release_details_public.py
+++ sbomify/apps/core/views/release_details_public.py
@@ -2,7 +2,6 @@ from django.http import HttpRequest, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.views import View
 
-from sbomify.apps.core.apis import get_release
 from sbomify.apps.core.errors import error_response
 from sbomify.apps.core.models import Release
 from sbomify.apps.teams.schemas import BrandingInfo
@@ -10,6 +9,12 @@ from sbomify.apps.teams.schemas import BrandingInfo
 
 class ReleaseDetailsPublicView(View):
     def get(self, request: HttpRequest, product_id: str, release_id: str) -> HttpResponse:
+        from sbomify.apps.core.apis import get_release
+
+        try:
+            release = Release.objects.get(pk=release_id)
+        except Release.DoesNotExist:
+            return error_response(request, HttpResponseNotFound("Release not found"))
 
         status_code, release_data = get_release(request, release_id)
         if status_code != 200:
