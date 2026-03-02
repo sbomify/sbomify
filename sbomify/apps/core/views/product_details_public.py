46fccf55de55277b9bb2e04170b3d2a5109c234d
Rana Aurangzaib <aurangzaib048@gmail.com>
2026-03-02 15:44:01 +0300

    remove redundant str() wrapper on Product ID (already a CharField)

--- sbomify/apps/core/views/product_details_public.py
+++ sbomify/apps/core/views/product_details_public.py
@@ -223,7 +223,7 @@ class ProductDetailsPublicView(View):
         passing_assessments = passing_assessments_to_dict(assessment_status.passing_assessments)
 
         # Build TEI URN if TEA is enabled with a validated custom domain
-        product_tei = get_product_tei_urn(str(resolved_id), product["team_id"])
+        product_tei = get_product_tei_urn(resolved_id, product["team_id"])
 
         context = {
             "brand": brand,
